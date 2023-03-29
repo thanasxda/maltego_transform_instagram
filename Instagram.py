773016f24bea349bc1c4d5db7ba1559ca58030b7
thanasxda <15927885+thanasxda@users.noreply.github.com>
2023-03-29 17:23:14 +0300

    test

--- Instagram.py
+++ Instagram.py
@@ -3,15 +3,16 @@
 # imports
 from maltego_trx.maltego import MaltegoMsg, MaltegoTransform
 from maltego_trx.transform import DiscoverableTransform
-import requests 
+import requests
+import json
 
-class Instagram(DiscoverableTransform):
+class instagram_data(DiscoverableTransform):
     
     @classmethod
     def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
         
         # obtain target info
-        request_username = request.getProperty("username")
+        request_username = request.getProperty("person.name")
         if bool(request_username) == False:
             request_username = ''
         
@@ -26,21 +27,21 @@ class Instagram(DiscoverableTransform):
         for i in instagram_data:
             
             # create instagram entity
-            instagram_data_entity = response.addEntity("thanasxda.Instagram")
+            instagram_data_entity = response.addEntity("maltego.affiliation.Instagram")
             
             # add properties to instagram entity
             response_username = i.get("username")
             if response_username:
-                instagram_data_entity.addProperty("username", value = response_username)
+                instagram_data_entity.addProperty("person.name", value = response_username)
             
-            response_full_name = i.get("full_name")
-            if response_full_name:
-                instagram_data_entity.addProperty("FullName", value = response_full_name)
+            #response_full_name = i.get("full_name")
+            #if response_full_name:
+            #    instagram_data_entity.addProperty("FullName", value = response_full_name)
 
-            response_fbid = i.get("fbid")
-            if response_fbid:
-                instagram_data_entity.addProperty("FbID", value = response_fbid)
+            #response_fbid = i.get("fbid")
+            #if response_fbid:
+            #    instagram_data_entity.addProperty("FbID", value = response_fbid)
                 
-            response_photo = i.get('profile_pic_url_hd')
-            if response_photo:
-                instagram_data_entity.addProperty("PhotoURL", value = response_photo['href'])
+            ##response_photo = i.get('profile_pic_url_hd')
+            ##if response_photo:
+            ##    instagram_data_entity.addProperty("PhotoURL", value = response_photo['href'])
